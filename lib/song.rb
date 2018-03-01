233648db1000d6bf3dbf2f173c8bc98449dd70bb
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 10:15:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,6 +25,7 @@ class Song
       artist = file.split(" - ")[0] #splits file at dashes, takes the first index (artist)
       song = file.split(" - ")[1] #takes second index
       new_song = self.new(song) #initialize with song from line 21
+      new_artist = 
       new_song.artist_name = artist
       new_song.save
   end 
