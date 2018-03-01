d5eb4734fbb4ff71e2505cd90bc5b52124eac90c
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 09:59:15 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs 
+  
+  #all artist instances stored in @@all
+  @@all = [] 
+  
+  #create every instance with the artist's name and an array to store that artist's songs 
+  def initialize(name)
+    @name = name #the artist's name
+    @songs = [] #the artist's collection of songs
+  end 
+  
+  #Artist class method that returns every artist instance created 
+  def self.all
+    @@all
+  end 
+
+  #instance method that adds an artist's song to his/her own songs array; returns the artist's songs array
+  def add_song(song)
+    @songs << song 
+    @songs 
+  end 
+  
+  #instance method that adds a particular artist instance to @@all array 
+  def save
+    @@all << self
+  end
+  
+  def self.find(name)
+    self.all.detect do |artist|
+      artist.name == name 
+    end
+  end 
+  
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end 
+  
+  #class method that loops through @@all to find a particular artist. If artist is detected in @@all, return that artist's name. Else create a new artist instance. 
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else 
+      self.create(name)
+        #self.all.detect do |artist|
+      #if artist.name != name 
+       # self.new(name) 
+    end
+  end
+
+  #instance method that lists all of an artist's songs 
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end 
+  end 
+end 
---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class Song 
+  attr_accessor :name, :artist
+  
+  #keep all song instances here
+  @@all = []
+  
+  #initialize every song instance with a name 
+  def initialize(name)
+    @name = name
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def save
+    @@all << self 
+    self
+  end 
+  
+  #Class method that takes a file, parses filename info, and creates a new song instance with that filename 
+
+  def self.new_by_filename(file) 
+      artist = file.split(" - ")[0] #splits file at dashes, takes the first index (artist)
+      song = file.split(" - ")[1] #takes second index
+      new_song = self.new(song) #initialize with song from line 21
+      new_song.artist_name = artist
+      new_song.save
+  end 
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end 
+end 
