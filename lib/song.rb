ec8a452f825f22e357efbb6c844a38b76214689b
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-01 10:33:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
 
   def self.new_by_filename(file) 
       split_file = file.split(" - ")
-      song = self.new(split_file[1]) #initialize with song from line 21
+      song = self.new(split_file[1]) 
       artist = Artist.find_or_create_by_name(split_file[0])
       song.artist = artist
       artist.add_song(song)
